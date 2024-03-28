701e68711f897d8ba39ab864569ed1cb664082a2
Sagar Guney <113847560+SagarG2003@users.noreply.github.com>
2024-03-29 01:09:51 +0530

    index file added

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+  });
+
 app.get('/', (req, res) => res.send('Sagar Guney'));
 
 app.listen(port);
